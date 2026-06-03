2a6ab2524b5732a9479e5116b3bd8678e2a6c825
vedant <vedantpachauri@gmail.com>
2026-06-03 14:21:26 +0530

    Merge remote-tracking branch 'github2/master'
    # Conflicts:
    #	render.yaml

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # APPS
